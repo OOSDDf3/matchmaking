2d3f48134e6c8f21b66c9daef094042a76fc3a9d
Gideon Feenstra <gideon.feenstra@outlook.com>
2023-11-21 19:15:42 +0100

    Profielpagina

---
+++ LinkApplication/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using LinkApplication.Core;
+using LinkApplication.MVVM.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkApplication.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand InteressesViewCommand { get; set; }
+        public RelayCommand ProfileViewCommand { get; set; }
+        public HomeViewModel HomeVM { get; set; }
+        public InteressesViewModel InteressesVM { get; set; }
+        public ProfileViewModel ProfileVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();    
+            }
+        }
+
+        public MainViewModel() 
+        {
+            HomeVM = new HomeViewModel();
+            InteressesVM = new InteressesViewModel();
+            ProfileVM = new ProfileViewModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => 
+            {
+                CurrentView = HomeVM;
+            });
+
+            InteressesViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = InteressesVM;
+            });
+
+            ProfileViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ProfileVM;
+            });
+        }
+    }
+}
--- LinkApplication/obj/Debug/net6.0-windows/RegisterWindow.g.i.cs
+++ LinkApplication/obj/Debug/net6.0-windows/RegisterWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace LinkApplication {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/LinkApplication;V1.0.0.0;component/registerwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/LinkApplication;component/registerwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\RegisterWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);

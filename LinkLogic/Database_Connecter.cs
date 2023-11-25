972647271f407bb570d9e910564655f1efb91e94
JoasWeeda <s1190350@student.windesheim.nl>
2023-11-25 16:46:10 +0100

    Database Connecter
    Added database connection and methods to insert new account into db and checklogin.

--- LinkLogic/Database_Connecter.cs
+++ LinkLogic/Database_Connecter.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using Azure;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,58 +14,117 @@ namespace LinkApplication
 {
     public class Database_Connecter
     {
-        SqlConnectionStringBuilder? sqlConnectionStringBuilder;
+        DatabaseConnection dbCon = null;
+        
+        public Database_Connecter() {
+            ConnectToLocalDatabase();
+            //InsertAccount("Joas", "joas@hotmail.com", "securepassword", 70, "streetname 42", "Male", "English");
+            CheckLogin("Joas Weeda", "newpassword");
+            CheckLogin("Joas", "securepassword");
+            CheckLogin("Joas", "unsecurepassword");
+            ShowUserInformation("Joas Weeda", "newpassword", "SELECT * FROM Account WHERE name = @userName AND password = @password");
+            ShowUserInformation("Joas Weeda", "falsenewpassword", "SELECT * FROM Account WHERE name = @userName AND password = @password");
+            dbCon.Close();
+        }      
 
-        SqlConnection? sqlConnection;
-
-        public Database_Connecter() { 
-            ConnectToDatabase();
-            TestDatabase("SELECT name, email, address FROM Account");
-        }
-
-        protected void ConnectToDatabase()
+        protected void ConnectToLocalDatabase()
         {
-            sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
-            sqlConnectionStringBuilder.UserID = "SA";
-            sqlConnectionStringBuilder.Password = "@Matchingf3";
-            sqlConnectionStringBuilder.DataSource = "localhost";
-            sqlConnectionStringBuilder.InitialCatalog = "Matching";
-
-            sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
-
-            sqlConnection.Open();
-            Console.WriteLine("Connection made");
-            sqlConnection.Close();
+            dbCon = DatabaseConnection.Instance();
+            dbCon.Server = "localhost";
+            dbCon.Database = "matchmaking";
+            dbCon.UserName = "root";
+            dbCon.Password = "MyNewPassword";
         }
 
-        private void TestDatabase(string query)
+        protected void ConnectToServerDatabase()
+        {
+            dbCon = DatabaseConnection.Instance();
+            dbCon.Server = "localhost";
+            dbCon.Database = "matchmaking";
+            dbCon.UserName = "SA";
+            dbCon.Password = "@Matchingf3";
+        }        
+
+        public void InsertAccount(string name, string email, string password, int age, string address, string gender, string language)
         {
             try
-            {
-                using (sqlConnection)
+            {               
+                if (dbCon.IsConnect())
                 {
-                    sqlConnection.Open();
-                    Console.WriteLine("===================================");
-
-                    using (SqlCommand command = new SqlCommand(query, sqlConnection))
-                    {
-                        sqlConnection.Open();
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                Console.WriteLine($"Name: {0}, E-mail: {1}, Address: {2}", reader.GetString(0), reader.GetString(1), reader.GetString(2));
-                            }
-                        }
-                    }
-                    sqlConnection.Close();
+                    string query = "INSERT INTO `account` (`user_ID`, `name`, `email`, `password`, `age`, `address`, `gender`, `language`) VALUES (NULL, @na, @em, @pa, @ag, @ad, @ge, @la);";
+                    var cmd = new MySqlCommand(query, dbCon.Connection);
+                    cmd.Parameters.Add("@na", MySqlDbType.VarChar, 100).Value = name;
+                    cmd.Parameters.Add("@em", MySqlDbType.VarChar, 100).Value = email;
+                    cmd.Parameters.Add("@pa", MySqlDbType.VarChar, 100).Value = password;
+                    cmd.Parameters.Add("@ag", MySqlDbType.Int32, 4).Value = age;
+                    cmd.Parameters.Add("@ad", MySqlDbType.VarChar, 100).Value = address;
+                    cmd.Parameters.Add("@ge", MySqlDbType.VarChar, 10).Value = gender;
+                    cmd.Parameters.Add("@la", MySqlDbType.VarChar, 50).Value = language;
+                    Console.WriteLine(cmd.ExecuteNonQuery());
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
-            Console.ReadLine();
+        }
+
+        public bool CheckLogin(string userName, string password)
+        {
+            try
+            {
+                if (dbCon.IsConnect())
+                {
+                    string query = "SELECT Count(*) FROM Account WHERE name = @userName AND password = @password";
+                    var cmd = new MySqlCommand(query, dbCon.Connection);
+                    cmd.Parameters.AddWithValue("@userName", userName);
+                    cmd.Parameters.AddWithValue("@password", password);
+                    var count = cmd.ExecuteScalar();
+                    if(count.ToString().Equals("1"))
+                    {
+                        Console.WriteLine("Login succesfull");
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Login unsuccesfull");
+                        return false;
+                    }
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+        }
+
+        public void ShowUserInformation(string userName, string password, string query)
+        {
+            try
+            {
+                if (dbCon.IsConnect() & CheckLogin(userName, password))
+                {
+                    var cmd = new MySqlCommand(query, dbCon.Connection);
+                    cmd.Parameters.AddWithValue("@userName", userName);
+                    cmd.Parameters.AddWithValue("@password", password);
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write(reader.GetValue(i).ToString() + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }

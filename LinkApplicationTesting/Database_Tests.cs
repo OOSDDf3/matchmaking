167d4dd57aa9ef9c10188f86650ebd13ed2646b6
JoasWeeda <s1190350@student.windesheim.nl>
2024-01-09 20:30:11 +0100

    Update Database_Tests.cs

--- LinkApplicationTesting/Database_Tests.cs
+++ LinkApplicationTesting/Database_Tests.cs
@@ -1,18 +1,11 @@
 
-using LinkApplication;
-using Moq;
-
 namespace LinkApplicationTesting
 {
     public class Database_Tests
     {
-
-        Database_Connecter _connecter;
-
         [SetUp]
         public void Setup()
         {
-            _connecter = new Database_Connecter();
         }
 
         [Test]
